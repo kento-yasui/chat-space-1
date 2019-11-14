1bade4829df0fa64b9664e3d54d7c1f2cad474d9
masuzawa1 <57562777+masuzawa1@users.noreply.github.com>
2019-11-14 21:55:17 +0900

    add Constraints to messages migrate

--- db/migrate/20191114114943_create_messages.rb
+++ db/migrate/20191114114943_create_messages.rb
@@ -1,8 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
+      t.references :user_id, null: false, foreign_key: true
+      t.references :room_id, null: false, foreign_key: true
       t.timestamps
     end
+    belongs_to :user
+    belongs_to :room
   end
 end
