39cbcc6362f340bb1e244588c831cb6106c239d7
masuzawa1 <57562777+masuzawa1@users.noreply.github.com>
2019-11-14 20:58:36 +0900

    users_table edit

--- db/migrate/20191114114916_create_users.rb
+++ db/migrate/20191114114916_create_users.rb
@@ -1,8 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password, null: false
+      t.references :rooms, foreign_key: true
       t.timestamps
+      add_index :users, :name, unique: true
+      add_index :users, :email, unique: true
+      add_index :users, :password, unique: true
+      has_many :rooms
+      has_many :messages
+      has_many :rooms,throght: rooms_users
     end
   end
 end
