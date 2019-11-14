bac35ecb2c8bd8a1473ce37e2fad3156f0f58097
masuzawa1 <57562777+masuzawa1@users.noreply.github.com>
2019-11-14 21:54:51 +0900

    add Constraints to rooms migrate

--- db/migrate/20191114114930_create_rooms.rb
+++ db/migrate/20191114114930_create_rooms.rb
@@ -1,8 +1,12 @@
 class CreateRooms < ActiveRecord::Migration[5.0]
   def change
     create_table :rooms do |t|
-
+      t.string :roomname, null: false
       t.timestamps
     end
+    add_index :rooms, :roomname, unique: true
+    has_many :users
+    has_many :messages
+    has_many :users,through: rooms_users
   end
 end
