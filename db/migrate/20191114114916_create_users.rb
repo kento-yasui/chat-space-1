a32fba355da9edf9c7bd412ed46e469a3f6581d7
masuzawa1 <57562777+masuzawa1@users.noreply.github.com>
2019-11-14 21:53:56 +0900

    fix users migrate

--- db/migrate/20191114114916_create_users.rb
+++ db/migrate/20191114114916_create_users.rb
@@ -1,17 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :name, null: false
-      t.string :email, null: false
-      t.string :password, null: false
+      t.string :name, null: false, unique: true
+      t.string :email, null: false, unique: true
+      t.string :password, null: false, unique: true
       t.references :rooms, foreign_key: true
       t.timestamps
-      add_index :users, :name, unique: true
-      add_index :users, :email, unique: true
-      add_index :users, :password, unique: true
-      has_many :rooms
-      has_many :messages
-      has_many :rooms,throght: rooms_users
     end
+    has_many :rooms
+    has_many :messages
+    has_many :rooms,throght: rooms_users
   end
 end
