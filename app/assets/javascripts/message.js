22961230bf4cf58420353e4d7694cf4cbc8e8dd9
masuzawa1 <57562777+masuzawa1@users.noreply.github.com>
2019-12-11 21:06:13 +0900

    add build HTML to message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,23 @@
 $(function(){
+  function buildHTML(message){
+      var body = message.content ? `${message.content}` : "";
+      var image = message.image_url ? `<img src="${message.image_url}">` : ""
+      var html = `<div class="message">
+                    <div class="message__info">
+                      <p class="message__info__talker">
+                        ${message.user_name}
+                      </p>
+                      <p class="message__info__date">
+                        ${message.created_at}
+                      </p>
+                    </div>
+                      <p class="message__text">
+                        ${message.content}
+                      </p>
+                      ${image}
+                    </div>`
+    return html
+  }
   $("#new_message").on("submit", function(e){
     e.preventDefault();
     var formData = new FormData(this);
