53969b00c018bdca0c41d65e972d68987749180d
masuzawa1 <57562777+masuzawa1@users.noreply.github.com>
2019-12-15 20:09:20 +0900

    add ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,34 +1,66 @@
 $(function(){
   function buildHTML(message){
-      var body = message.content ? `${message.content}` : "";
-      var image = message.image_url ? `<img src="${message.image_url}">` : ""
-      var html = `<div class="message">
-                    <div class="message__info">
-                      <p class="message__info__talker">
-                        ${message.user_name}
-                      </p>
-                      <p class="message__info__date">
-                        ${message.created_at}
-                      </p>
-                    </div>
-                      <p class="message__text">
-                        ${message.content}
-                      </p>
-                      ${image}
-                    </div>`
-    return html
+    if (message.image !== null){img = '<img src ="${message.image.url}">'}
+      var img =
+        `<p class= "lower-message__content"></p>
+        <img src="${message.image}">`
+
+      var html =
+      `<div class="message" data-message-id="${message.id}">
+        <div class="message__info">
+          <div class="message__info__talker">
+            ${message.user_name}
+          </div>
+          <div class="message__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message__text">
+          <p class= "message__text__a">
+          ${message.content}
+          </p>
+          ${message.image == null ? "" : '<img src="' + message.image + '">'}
+        </div>
+      </div>`
+    // }else{
+    //   var html =
+    //   `<div class="message" data-message-id="${message.id}">
+    //     <div class="message__info">
+    //       <div class="message__info__talker">
+    //         ${message.user_name}
+    //       </div>
+    //       <div class="message__info__date">
+    //         ${message.created_at}
+    //       </div>
+    //     </div>
+    //       <div class="message__text">
+    //         ${message.content}
+    //       </div>
+    //   </div>`
+    // }
+    return html;
   }
   $("#new_message").on("submit", function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr("action")
     $.ajax({
       url: url,
-      type: 'POST',
+      type: "POST",
       data: formData,
-      dataType: 'json',
+      dataType: "json",
       processData: false,
-      contentType: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $(".messages").append(html);
+      $("#new_message")[0].reset();
+      $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight})
+      $(".send__submit-btn").prop("disabled", false);
+    })
+    .fail(function(data){
+      alert("エラーが発生したため送信できませんでした");
     })
   })
 })
